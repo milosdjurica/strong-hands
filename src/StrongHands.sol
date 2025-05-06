2a844df904ce0596219f2ec7fd6502c8eaa89694
milosdjurica <milosdjurica.work@gmail.com>
2025-05-06 16:14:21 +0200

    started writting contract logic

--- src/StrongHands.sol
+++ src/StrongHands.sol
@@ -3,11 +3,20 @@ pragma solidity ^0.8.13;
 
 contract StrongHands {
     error StrongHands__NotOwner(address msgSender, address owner);
+    error StrongHands__ZeroDeposit();
+    error StrongHands__ZeroAmount();
 
     // TODO ->  events for deposit, withdraw and claimInterest
 
     uint256 public immutable i_lockPeriod;
     address public immutable i_owner;
+
+    struct User {
+        uint256 amount;
+        uint256 lastTimeDeposited;
+    }
+
+    mapping(address => User) users;
 
     constructor(uint256 _lockPeriod) {
         i_lockPeriod = _lockPeriod;
@@ -19,17 +28,23 @@ contract StrongHands {
         _;
     }
 
+    // can deposit multiple times
+    // depositing starts new lock period counting for user
     function deposit() external payable {
-        // can deposit multiple times
-        // depositing starts new lock period counting for user
-        // penalty goes from 50% at start to the 0% at the end of lock period
+        if (msg.value == 0) revert StrongHands__ZeroDeposit();
+
+        users[msg.sender].amount += msg.value;
+        users[msg.sender].lastTimeDeposited = block.timestamp;
     }
 
+    // must withdraw all, can not withdraw partially
+    // penalty goes from 50% at start to the 0% at the end of lock period
     function withdraw() external {
-        // must withdraw all, can not withdraw partially
+        if (users[msg.sender].amount == 0) revert StrongHands__ZeroAmount();
+
+        users[msg.sender].amount = 0;
     }
 
-    function claimInterest() external onlyOwner {
-        // ONLY OWNER can call this function. He can call it at any moment
-    }
+    // ONLY OWNER can call this function. He can call it at any moment
+    function claimInterest() external onlyOwner {}
 }
