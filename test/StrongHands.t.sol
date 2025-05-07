0dc05073327a76aea74d654d691d0ed9d9e80a8d
milosdjurica <milosdjurica.work@gmail.com>
2025-05-07 14:38:02 +0200

    withdraw tests

--- test/StrongHands.t.sol
+++ test/StrongHands.t.sol
@@ -13,6 +13,7 @@ contract StrongHandsTest is Test {
     address ALICE = makeAddr("ALICE");
 
     event Deposited(address indexed sender, uint256 indexed amount, uint256 indexed timestamp);
+    event Withdrawn(address indexed user, uint256 indexed payout, uint256 indexed penalty, uint256 timestamp);
 
     function setUp() public {
         deployScript = new StrongHandsDeploy();
@@ -42,6 +43,7 @@ contract StrongHandsTest is Test {
 
         assertEq(amount, 1 ether);
         assertEq(timestamp, block.timestamp);
+        assertEq(strongHands.totalStaked(), 1 ether);
     }
 
     function testFuzz_deposit(uint256 amountToDeposit) public {
@@ -56,7 +58,78 @@ contract StrongHandsTest is Test {
 
         assertEq(amount, amountToDeposit);
         assertEq(timestamp, block.timestamp);
+        assertEq(strongHands.totalStaked(), amountToDeposit);
     }
 
     // ! Withdraw tests
+    function test_withdraw_RevertIf_ZeroAmount() public {
+        vm.expectRevert(abi.encodeWithSelector(StrongHands.StrongHands__ZeroAmount.selector));
+        strongHands.withdraw();
+    }
+
+    // Helper modifier
+    modifier depositWithBob() {
+        vm.prank(BOB);
+        strongHands.deposit{value: 1 ether}();
+        _;
+    }
+
+    function test_withdraw_ZeroFee() public depositWithBob {
+        skip(deployScript.LOCK_PERIOD());
+        vm.prank(BOB);
+        vm.expectEmit(true, true, true, true);
+        emit Withdrawn(BOB, 1 ether, 0, block.timestamp);
+        strongHands.withdraw();
+
+        (uint256 amount,) = strongHands.users(BOB);
+        assertEq(amount, 0);
+        // assertEq(timestamp, block.timestamp);
+        assertEq(strongHands.totalStaked(), 0);
+    }
+
+    function test_withdraw_MaxFee() public depositWithBob {
+        vm.prank(BOB);
+        vm.expectEmit(true, true, true, true);
+        emit Withdrawn(BOB, 0.5 ether, 0.5 ether, block.timestamp);
+        strongHands.withdraw();
+
+        (uint256 amount,) = strongHands.users(BOB);
+        assertEq(amount, 0);
+        // assertEq(timestamp, block.timestamp);
+        assertEq(strongHands.totalStaked(), 0);
+    }
+
+    function test_withdraw_MidFee() public depositWithBob {
+        skip(deployScript.LOCK_PERIOD() / 2);
+        vm.prank(BOB);
+        vm.expectEmit(true, true, true, true);
+        emit Withdrawn(BOB, 0.75 ether, 0.25 ether, block.timestamp);
+        strongHands.withdraw();
+
+        (uint256 amount,) = strongHands.users(BOB);
+        assertEq(amount, 0);
+        // assertEq(timestamp, block.timestamp);
+        assertEq(strongHands.totalStaked(), 0);
+    }
+
+    function testFuzz_withdraw_RandomFee(uint256 timePassed) public depositWithBob {
+        timePassed = bound(timePassed, 0, deployScript.LOCK_PERIOD());
+        skip(timePassed);
+        uint256 timeLeft = deployScript.LOCK_PERIOD() - timePassed;
+
+        uint256 expectedFee = 1 ether * timeLeft / deployScript.LOCK_PERIOD() * 50 / 100;
+        uint256 expectedPayout = 1 ether - expectedFee;
+
+        vm.prank(BOB);
+        vm.expectEmit(true, true, true, true);
+        emit Withdrawn(BOB, expectedPayout, expectedFee, block.timestamp);
+        strongHands.withdraw();
+
+        (uint256 amount,) = strongHands.users(BOB);
+        assertEq(amount, 0);
+        // assertEq(timestamp, block.timestamp);
+        assertEq(strongHands.totalStaked(), 0);
+    }
+
+    // TODO -> Tests with multiple deposits, multiple withdraws, combinations, same user deposits many times
 }
